5d046e19e130e09e0709cc4a88a86c8792f2c9f4
Yasunari Iguchi <iguchi.yasunari@jp.panasonic.com>
2020-08-13 01:10:36 +0900

    add write to log.csv

--- end_water.js
+++ end_water.js
@@ -1,7 +1,7 @@
 const googlehome = require('google-home-notifier');
 const language = 'ja';
 googlehome.device("Google-Home", language);
-googlehome.ip("192.168.0.10");
+googlehome.ip("192.168.0.30");
 googlehome.notify('水やりが終わりました。', function(res) {
   console.log(res);
 });
--- read_arduino.py
+++ read_arduino.py
@@ -1,11 +1,21 @@
 import serial
 import datetime
+import csv
 ser = serial.Serial('/dev/ttyACM0', 115200)
 while True:
     Int_data = int(ser.readline())
-    print(datetime.datetime.now())
+    time_now = datetime.datetime.now()
+    print(time_now)
     print(Int_data)
     print()
+
+    l =[time_now, Int_data]
+
+    with open('/home/pi/Documents/water_projects/log.csv', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow(l)
+        
+        
     
     """
     if Int_data <= 807:
