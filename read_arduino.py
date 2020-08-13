a3ca2af988faa166d9ba5e37bf06ad2d0b7350e2
Yasunari Iguchi <iguchi.yasunari@jp.panasonic.com>
2020-08-13 22:37:53 +0900

    connect read_arduino.py and water.py

--- read_arduino.py
+++ read_arduino.py
@@ -1,25 +1,51 @@
 import serial
 import datetime
 import csv
+import subprocess
+import sys
+dry_thresh = 850
+emergency_thresh = 600
 ser = serial.Serial('/dev/ttyACM0', 115200)
 while True:
+    print('waiting for data...')
     Int_data = int(ser.readline())
     time_now = datetime.datetime.now()
-    print(time_now)
-    print(Int_data)
+    print('time:', time_now)
+    print('data:', Int_data)
+    if Int_data >= dry_thresh :
+        print('water process started')
+        subprocess.call("python /home/pi/Documents/water_projects/water.py 2",shell=True)
+        Int_data_before = Int_data
+        print('water process finished, waiting for data_after...')
+        Int_data_after = int(ser.readline())
+        l =[time_now, Int_data_before, Int_data_after]
+
+        with open('/home/pi/Documents/water_projects/log.csv', 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow(l)
+        print('time:', time_now)
+        print('data_before:', Int_data_before)
+        print('data_after:', Int_data_after)
+        print('memorized')
+        if Int_data_after >= emergency_thresh :
+            print('emergency occured')
+            with open('/home/pi/Documents/water_projects/log.csv', 'a') as f:
+                writer = csv.writer(f)
+                writer.writerow('emergency')
+            print('quit')
+            sys.exit()
+        else:
+            print('giving water succeeded')
+    else:
+        print('wet enough')
     print()
 
-    l =[time_now, Int_data]
-
-    with open('/home/pi/Documents/water_projects/log.csv', 'a') as f:
-        writer = csv.writer(f)
-        writer.writerow(l)
-        
         
     
     """
     if Int_data <= 807:
         print('chiisai')
+
     else:
         print('ookii')
     """
